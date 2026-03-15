ef67082a14f68189f291475acc9d9ee5a82340fa
Vincent COURTOIS <vincent.courtois@ecov.fr>
2026-03-15 09:27:56 +0100

    Add the application and its swagger to the docker compose file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeSwagger } from '@synple/utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  initializeSwagger(
+    app,
+    'Synple API',
+    'The API used to created, read, update or delete elements mainly linked to synthesizers and module templates',
+    '0.0.0'
+  )
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
--- apps/api/src/tools/tools.controller.ts
+++ apps/api/src/tools/tools.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller('/')
+@ApiTags('Tools')
 export class ToolsController {
   @Post('/')
   public async create() {
