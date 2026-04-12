b3ed28c77365a30d3657aae4c0a48c02289dec30
Vincent Courtois <courtois.vincent@outlook.com>
2026-04-12 23:55:06 +0200

    Add the pipe and interceptor needed for blueprint creation

--- apps/api/src/blueprints/blueprints.controller.ts
+++ apps/api/src/blueprints/blueprints.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, HttpCode, HttpStatus, Post, UseInterceptors, UsePipes } from "@nestjs/common";
 import { CreateBlueprintDto } from "./dto/create-blueprint.dto";
 import { BlueprintsService } from "@synple/common/services/blueprints.service";
+import { transformErrorPipe } from "@synple/common";
 
 @Controller('/:uuid/blueprints')
 export class BlueprintsController {
@@ -9,6 +10,8 @@ export class BlueprintsController {
 
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UsePipes(transformErrorPipe)
   public async create(@Body() { name, slots }: CreateBlueprintDto) {
     return await this.service.create({ name, slots })
   }
--- apps/api/src/blueprints/blueprints.module.ts
+++ apps/api/src/blueprints/blueprints.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { BlueprintsController } from "./blueprints.controller";
+import { BlueprintsService } from "@synple/common/services/blueprints.service";
 
 @Module({
-  controllers: [ BlueprintsController]
+  controllers: [ BlueprintsController],
+  providers: [ BlueprintsService ],
 })
 export class BlueprintsModule { }
