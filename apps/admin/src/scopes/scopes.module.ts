536160b6aeca44d20683c9b65d833218b7e82963
Vincent Courtois <courtois.vincent@outlook.com>
2026-04-14 10:16:41 +0200

    Remove unused module for services

--- apps/admin/src/scopes/scopes.module.ts
+++ apps/admin/src/scopes/scopes.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScopesController } from './scopes.controller';
 import { ScopesService } from '@synple/common/services/admin/scopes.service';
 import { AccountsService, TokensService } from '@synple/common';
-import { ServicesModule } from '@synple/common/services/services.module';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [ScopesController],
   providers: [ScopesService, JwtService, TokensService, AccountsService],
-  imports: [ServicesModule],
 })
 export class ScopesModule {}
--- libs/common/src/services/services.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-
-@Module({
-  providers: [JwtService],
-})
-export class ServicesModule {}
